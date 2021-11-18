8edc46bf9b0f1f1f43aaec16602401a3adfafd36
Taylor Varoglu <tava8398@colorado.edu>
2021-11-18 15:46:32 -0700

    Create Post table

--- app/models.py
+++ app/models.py
@@ -7,6 +7,18 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
+        # return '<User {}>'.format(self.username)
         return f'<User {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    created_at = db.Column(db.DateTime, nullable=False, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        # return '<Post {}>'.format(self.body)
+        return f'<Post {self.body}>'
