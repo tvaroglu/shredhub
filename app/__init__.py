d595a38384b57c3a3e766b828441f56e425f8c47
Taylor Varoglu <tava8398@colorado.edu>
2021-11-18 20:52:58 -0700

    Add login and logout to routes and forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # see docs for more detail on .from_object() function:
@@ -9,5 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 # import routes at END of file to prevent circular imports
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,51 @@
-from flask import render_template
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
-# View functions with decorators added to create an association between the URL (arg) and associated function call:
 @app.route('/')
 @app.route('/index')
+# decorator that protects app (function call) from unauthenticated ('anonymous') current_user
+@login_required
 def index():
-    # TODO: remove mock user and post objects once DB & auth in place
-    user = {'username': 'Taylor'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': "Snow's coming this week!"
-        },
-        {
-            'author': {'username': 'Natalie'},
-            'body': "Can't wait to check out Steamboat!"
-        }
-    ]
-    # Jinja2 template engine (bundled with Flask) renders content dynamically to templates via passed in args:
+    # Jinja2 template engine (Flask extension) renders content dynamically to templates via passed in args:
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data.lower())
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for joining the party, fellow Shredder!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.email.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.email.data.lower()).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        # determine if URL is relative vs absolute, and parse with Werkzeug.url_parse() to check if netloc component is set:
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
