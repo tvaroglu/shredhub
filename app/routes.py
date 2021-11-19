dcd5e7136fa778262e8364147241e385cdc23ed9
Taylor Varoglu <tava8398@colorado.edu>
2021-11-19 08:49:12 -0700

    Add basic auth

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from werkzeug.urls import url_parse
 @login_required
 def index():
     # Jinja2 template engine (Flask extension) renders content dynamically to templates via passed in args:
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=current_user, posts=current_user.posts)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -19,7 +19,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data.lower())
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
@@ -33,9 +33,9 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.email.data.lower()).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid email or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
