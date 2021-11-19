a98d81602d8f28eb23345d3f9cafdd2c78e1963b
Taylor Varoglu <tava8398@colorado.edu>
2021-11-19 16:48:42 -0700

    Add rotating file handler for logging

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
