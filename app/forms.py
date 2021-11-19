e8e113ca4d38a88890478577d727a546b74e95df
Taylor Varoglu <tava8398@colorado.edu>
2021-11-19 09:13:30 -0700

    Refactor validations for user auth

--- app/forms.py
+++ app/forms.py
@@ -19,11 +19,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        user = User.query.filter_by(username=username.data.lower()).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = User.query.filter_by(email=email.data.lower()).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
