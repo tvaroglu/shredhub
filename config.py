e323a6a242acda988c8f0a8f51875eaeb4fee757
Taylor Varoglu <tava8398@colorado.edu>
2021-11-27 16:12:34 -0700

    Add pagination to index

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
