c5aa02c4ffb537c58d8d4446dc144f9e65328be6
Taylor Varoglu <taylor.varoglu@gmail.com>
2022-12-04 12:12:46 -0700

    Update shredhub.py for docker postgres image

--- shredhub.py
+++ shredhub.py
@@ -1,13 +1,22 @@
 from app import app, db, mail
 # from flask_mail import Message
 from app.models import User, Post, Message
+from flask.cli import FlaskGroup
+
+cli = FlaskGroup(app)
+@cli.command('create_db')
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 # decorator to register the function as a shell context function, to auto-import modules for db manipulation
 # return dict (vs list) to provide a name under which each obj is referenced in the shell (via dict keys):
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'mail': mail, 'User': User, 'Post': Post, 'Message': Message, 'seed': seed}
+    return {'db': db, 'mail': mail, 'User': User, 'Post': Post, 'Message': Message, 'reset_db': reset_db}
 
-def seed():
+def reset_db():
     imports = {'db': db, 'User': User, 'Post': Post, 'Message': Message}
     users = User.query.all()
     posts = Post.query.all()
@@ -20,3 +29,6 @@ def seed():
     for m in messages:
         db.session.delete(m)
     db.session.commit()
+
+if __name__ == 'main':
+    cli()
